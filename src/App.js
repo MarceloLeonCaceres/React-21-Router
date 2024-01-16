a30c17e7e4e9ae4500060e1332533948a73ce2c9
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-16 15:48:11 -0500

    450 Showing Error Pages with errorElement

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route
-      path=""
-      element={<RootLayout />}
-      children={[
-        <Route path="/" element={<HomePage />} />,
-        <Route path="/products" element={<ProductsPage />} />,
-      ]}
-    />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route
+//       path=""
+//       element={<RootLayout />}
+//       errorElement={<ErrorPage/>}
+//       children={[
+//         <Route path="/" element={<HomePage />} />,
+//         <Route path="/products" element={<ProductsPage />} />,
+//       ]}
+//     />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage/> },
-//   { path: '/products', element: <ProductsPage />}
-// ]);
+// const router = createBrowserRouter(routeDefinitions);
 
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
